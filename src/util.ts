993d5ed7472c64d4139ce0ae83537017bfc17bc3
jeffreyvl <jeffreyvl.github@outlook.com>
2018-05-31 01:20:04 +0200

    save

--- src/util.ts
+++ src/util.ts
@@ -3,5 +3,7 @@ export enum ToolbarOptions { Simple, Full }
 export enum PasteOptions { Full, PlainText, MSWord }
 export enum Direction {LTR,RTL}
 export enum CaretPosition {Inherit,Before,After}
+export enum NodesInRange {All,Inside, Intersecting, ChildrenAll, ChildrenInside}
+
 export class Dictionary<T> { [key: string]: T; }
 
