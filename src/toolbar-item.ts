654526fcc4c3a5e7ab06a2e0e3a7908372ca660c
jeffreyvl <jeffreyvl.github@outlook.com>
2018-05-31 20:38:32 +0200

    paragraph insert

--- src/toolbar-item.ts
+++ src/toolbar-item.ts
@@ -26,9 +26,9 @@ export class ColorPickerItem extends ToolbarItem {
         this.command = command?command:name;
         this.color = defaultColor;
         this.applyButton = $("<button/>").appendTo(this.container).attr("title", text).addClass(name)
-                            .css("float", "left").click((e) => {this.execute();e.preventDefault();return false;})[0];
+                            .css("float", "left").click((e) => {e.preventDefault();this.execute();return false;})[0];
         this.selectButton = $("<button/>").appendTo(this.container).attr("title", text).addClass("selector")
-                            .click((e) => {this.openColorPicker(); e.preventDefault();})[0];
+                            .click((e) => {e.preventDefault();this.openColorPicker();})[0];
         this.showColor();
     }
 
@@ -42,4 +42,35 @@ export class ColorPickerItem extends ToolbarItem {
     openColorPicker(): void {
         return;
     }
+}
+
+export class DropwdownItem extends ToolbarItem {
+
+    command:string;
+    options: [[string|number, string]];
+    dropDownList: HTMLElement;
+    constructor(name:string,text:string, toolbar:Toolbar, options: [[string|number, string]], command?:string) {
+        super(toolbar);
+        this.command = command;
+        this.options = options;
+        $(this.container).append(text+"  ").addClass(name);
+        this.dropDownList = $("<select/>").appendTo(this.container).change((e)=> {e.preventDefault();this.execute();return false;})[0];
+        options.forEach((val) => {
+            $(this.dropDownList).append($("<option></option>")
+                           .attr("value",val[0])
+                           .text(val[1]));
+       });
+    }
+
+    getCurrentValue():string|number {
+        return (<HTMLSelectElement>this.dropDownList).value;
+    }
+    execute(): void {
+        let currentValue: string|number = this.getCurrentValue();
+        if (currentValue === "default") {
+            this.toolbar.editArea.formatDoc("removeformat", false, this.command);
+        }
+        this.toolbar.editArea.formatDoc(this.command, false, currentValue);
+    }
+
 }
