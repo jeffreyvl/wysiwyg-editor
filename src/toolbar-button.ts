42b40415e7a015a178e82e23fd89df01242283af
jeffreyvl <jeffreyvl.github@outlook.com>
2018-06-04 11:08:32 +0200

    last features color picker and modal left

--- src/toolbar-button.ts
+++ src/toolbar-button.ts
@@ -1,4 +1,3 @@
-
 import { Direction, ActiveMode } from "./util";
 import { ToolbarItem, ItemToCheck } from "./toolbar-item";
 import { Toolbar } from "./toolbar";
@@ -9,7 +8,8 @@ export abstract class ToolbarButtonBase extends ToolbarItem {
     constructor(name: string, text: string, toolbar: Toolbar) {
         super(toolbar);
         this.button = $("<button/>").addClass(name).attr("title", text).click((e) => {
-            e.preventDefault();this.execute();this.toolbar.checkState();return false;})[0];
+            e.preventDefault(); this.execute(); this.toolbar.checkState(); return false;
+        })[0];
         $(this.container).append(this.button);
     }
 
@@ -49,12 +49,74 @@ export class ToolbarButtonExecCommandCheck extends ToolbarButtonExecCommand impl
 
 export class ToggleViewButton extends ToolbarButtonBase {
 
-    activeMode:ActiveMode;
-    constructor(activeMode:ActiveMode, name:string, text:string, toolbar:Toolbar) {
-        super(name,text, toolbar);
+    activeMode: ActiveMode;
+    constructor(activeMode: ActiveMode, name: string, text: string, toolbar: Toolbar) {
+        super(name, text, toolbar);
         this.activeMode = activeMode;
     }
-    execute():boolean {
+    execute(): boolean {
         return this.toolbar.setMode(this.activeMode);
+    }
+}
+
+export class CreateLink extends ToolbarButtonBase {
+
+    div: HTMLElement;
+    select:HTMLElement;
+    textBox:HTMLElement;
+    prompt: any;
+    constructor(name: string, text: string, toolbar: Toolbar) {
+        super(name, text, toolbar);
+        this.div = $("<div/>")[0];
+        this.createDiv();
+        this.createPrompt();
+    }
+
+    execute(): boolean {
+        let url: string;
+        let target: string;
+        this.prompt.dialog("open");
+        return true;
+    }
+    createPrompt(): void {
+        let that:CreateLink = this;
+        this.prompt = $("#dialog-form").dialog({
+            autoOpen: false,
+            height: 400,
+            width: 350,
+            modal: true,
+            buttons: {
+                "OK": that.createLink,
+                Cancel: () => {
+                    that.prompt.dialog("close");
+                }
+            },
+            close: () => {
+                form[0].reset();
+            }
+        });
+
+        let form: any = this.prompt.find("form").on("submit", event => {
+            event.preventDefault();
+            this.createLink();
+        });
+
+    }
+
+    createLink(): void {
+        this.toolbar.editArea.createLink(<string>$(this.textBox).val(),<string>$(this.select).val());
+    }
+    createDiv(): void {
+        this.textBox = $("<input/>").attr("input", "text").attr("id", "url").attr("maxLength", "225")
+            .attr("value", "https://").css("width", "200px")[0];
+        this.select = $("<select/>").attr("id", "target").css("width", "105px")
+            .append("<option selected value=_blank>New window</option>")
+            .append("<option value=_self>Current window</option>")
+            .append("<option value=_parent>Parent window</option>")
+            .append("<option value=_top>Top window</option>")[0];
+        // let cancelButton: HTMLElement = $("<button/>").attr("type", "button").addClass("cancel").attr("id", "cancel").text("Cancel")[0];
+        // let okButton: HTMLElement = $("<button/>").attr("type", "submit").addClass("submit").attr("id", "submit").text("OK")[0];
+        $("<form/>").appendTo(this.div).append("URL:").append(this.textBox).append("Target:")
+            .append(this.select);
     }
 }
--- src/toolbar-item.ts
+++ src/toolbar-item.ts
@@ -1,17 +1,18 @@
 import { EditArea } from "./edit-area";
 import { Toolbar } from "./toolbar";
+import { Dictionary } from "./util";
 
 export abstract class ToolbarItem {
-    container:HTMLElement;
+    container: HTMLElement;
     toolbar: Toolbar;
-    constructor(toolbar:Toolbar) {
+    constructor(toolbar: Toolbar) {
         this.toolbar = toolbar;
         this.container = document.createElement("li");
     }
 }
 export interface ItemToCheck {
 
-    checkState():void;
+    checkState(): void;
 }
 
 export class ColorPickerItem extends ToolbarItem {
@@ -19,24 +20,24 @@ export class ColorPickerItem extends ToolbarItem {
     selectButton: HTMLElement;
     applyButton: HTMLElement;
     color: string;
-    command:string;
+    command: string;
 
-    constructor(name: string, text: string, toolbar: Toolbar, defaultColor: string, command?:string) {
+    constructor(name: string, text: string, toolbar: Toolbar, defaultColor: string, command?: string) {
         super(toolbar);
-        this.command = command?command:name;
+        this.command = command ? command : name;
         this.color = defaultColor;
         this.applyButton = $("<button/>").appendTo(this.container).attr("title", text).addClass(name)
-                            .css("float", "left").click((e) => {e.preventDefault();this.execute();return false;})[0];
+            .css("float", "left").click((e) => { e.preventDefault(); this.execute(); return false; })[0];
         this.selectButton = $("<button/>").appendTo(this.container).attr("title", text).addClass("selector")
-                            .click((e) => {e.preventDefault();this.openColorPicker();})[0];
+            .click((e) => { e.preventDefault(); this.openColorPicker(); })[0];
         this.showColor();
     }
 
-    showColor():void {
+    showColor(): void {
         $(this.applyButton).css("background-color", this.color);
     }
     execute(): void {
-        this.toolbar.editArea.formatDoc(this.command,false,this.color);
+        this.toolbar.editArea.formatDoc(this.command, false, this.color);
     }
 
     openColorPicker(): void {
@@ -44,33 +45,54 @@ export class ColorPickerItem extends ToolbarItem {
     }
 }
 
-export class DropwdownItem extends ToolbarItem {
+export class DropwdownItem extends ToolbarItem implements ItemToCheck {
 
-    command:string;
-    options: [[string|number, string]];
+    command: string;
+    options: Dictionary<string | number>;
     dropDownList: HTMLElement;
-    constructor(name:string,text:string, toolbar:Toolbar, options: [[string|number, string]], command?:string) {
+    constructor(name: string, text: string, toolbar: Toolbar, options: Dictionary<string | number>, command?: string) {
         super(toolbar);
         this.command = command;
         this.options = options;
-        $(this.container).append(text+"  ").addClass(name);
-        this.dropDownList = $("<select/>").appendTo(this.container).change((e)=> {e.preventDefault();this.execute();return false;})[0];
-        options.forEach((val) => {
-            $(this.dropDownList).append($("<option></option>")
-                           .attr("value",val[0])
-                           .text(val[1]));
-       });
+        $(this.container).append(text + "  ").addClass(name);
+        this.dropDownList = $("<select/>").appendTo(this.container).change((e) => { e.preventDefault(); this.execute(); return false; })[0];
+
+        // tslint:disable-next-line:forin
+        for (let key in this.options) {
+            $(this.dropDownList).append($("<option/>")
+                .attr("value", this.options[key])
+                .text(key));
+        }
     }
 
-    getCurrentValue():string|number {
+    getCurrentValue(): string | number {
         return (<HTMLSelectElement>this.dropDownList).value;
     }
+
+    checkState(): void {
+        let documentValue: string|number = this.toolbar.editArea.getFont(this.command);
+        let flag: boolean = false;
+         // tslint:disable-next-line:forin
+         for (let key in this.options) {
+             if (key.toLowerCase() === documentValue || this.options[key] === documentValue) {
+                 flag = true;
+                 documentValue = this.options[key];
+                 break;
+             }
+         }
+        if (flag) {
+            $(this.dropDownList).val(documentValue);
+        } else {
+            $(this.dropDownList).val("default");
+        }
+    }
     execute(): void {
-        let currentValue: string|number = this.getCurrentValue();
+        let currentValue: string | number = this.getCurrentValue();
         if (currentValue === "default") {
             this.toolbar.editArea.formatDoc("removeformat", false, this.command);
+        } else {
+            this.toolbar.editArea.formatDoc(this.command, false, currentValue);
         }
-        this.toolbar.editArea.formatDoc(this.command, false, currentValue);
     }
 
 }
